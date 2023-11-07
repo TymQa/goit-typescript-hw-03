5b3c045eebd16046e23c8155ef6b4b77e8f18b64
Tymur Qayoom <tymursheikh4576@gmail.com>
2023-11-07 20:11:39 +0200

    +

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,56 @@
+interface IKey {
+  getSignature(): number;
+}
+
+class Key implements IKey {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  public getSignature(this: Key) {
+    return this.signature;
+  }
+}
+
+interface IPerson {
+  getKey(): number;
+}
+
+class Person implements IPerson {
+  constructor(private key: Key) {}
+
+  public getKey(this: Person) {
+    return this.key.getSignature();
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    } else {
+      throw new Error("Door is closed");
+    }
+  }
+
+  public abstract openDoor(key: number): void;
+}
+
+class MyHouse extends House {
+  public openDoor(key: number): void {
+    if (key === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +59,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
